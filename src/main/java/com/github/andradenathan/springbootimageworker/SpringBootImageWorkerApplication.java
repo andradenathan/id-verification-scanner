f815af1686cfa4b569f5514ebda822b5346c8d47
Nathan Andrade <nathan.nhdsk@gmail.com>
2025-12-18 12:39:38 -0300

    chore: first commit

---
+++ src/main/java/com/github/andradenathan/springbootimageworker/SpringBootImageWorkerApplication.java
@@ -0,0 +1,11 @@
+package com.github.andradenathan.springbootimageworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootImageWorkerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootImageWorkerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/github/andradenathan/springbootimageworker/SpringBootImageWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.andradenathan.springbootimageworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootImageWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
